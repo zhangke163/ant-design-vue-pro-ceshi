26e9c80b589b5b9adcf83dea49c76e8cd26c40af
张可 <zhangke@zjzhd.com>
2019-09-23 09:15:43 +0800

    菜单新增

--- src/layouts/SiderMenu.vue
+++ src/layouts/SiderMenu.vue
@@ -1,68 +1,96 @@
 <template>
   <div style="width: 256px">
     <a-menu
-      :defaultSelectedKeys="['1']"
-      :defaultOpenKeys="['2']"
+      :selectedKeys="selectedKeys"
+      :openKeys.sync="openKeys"
       mode="inline"
       :theme="theme"
-      :inlineCollapsed="collapsed"
     >
-      <template v-for="item in list">
-        <a-menu-item v-if="!item.children" :key="item.key">
-          <a-icon type="pie-chart" />
-          <span>{{item.title}}</span>
+      <template v-for="item in menuData">
+        <a-menu-item
+          v-if="!item.children"
+          :key="item.path"
+          @click="() => $router.push({ path: item.path, query: $route.query })"
+        >
+          <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
+          <span>{{ item.meta.title }}</span>
         </a-menu-item>
-        <sub-menu v-else :menu-info="item" :key="item.key"/>
+        <sub-menu v-else :menu-info="item" :key="item.path" />
       </template>
     </a-menu>
   </div>
 </template>
 
 <script>
-/* 
+/*
  * recommend SubMenu.vue https://github.com/vueComponent/ant-design-vue/blob/master/components/menu/demo/SubMenu.vue
  * SubMenu1.vue https://github.com/vueComponent/ant-design-vue/blob/master/components/menu/demo/SubMenu1.vue
  * */
-import SubMenu from './SubMenu'
+import SubMenu from "./SubMenu";
 export default {
-	props: {
-		theme: {
-			type: String,
-			default: 'dark'
-		},
-	},
-  components: {
-    'sub-menu': SubMenu,
-  },
-  data () {
-    return {
-      collapsed: false,
-      list: [
-        {
-          key: '1',
-          title: 'Option 1',
-        }, {
-          key: '2',
-          title: 'Navigation 2',
-          children: [
-            {
-              key: '2.1',
-              title: 'Navigation 3',
-              children: [
-                { key: '2.1.1',
-                  title: 'Option 2.1.1',
-                },
-              ],
-            },
-          ],
-        }],
+  props: {
+    theme: {
+      type: String,
+      default: "dark"
     }
   },
-  methods: {
-    toggleCollapsed () {
-      this.collapsed = !this.collapsed
-    },
+  components: {
+    "sub-menu": SubMenu
   },
-}
+  watch: {
+    "$route.path": function(val) {
+      this.selectedKeys = this.selectedKeysMap[val];
+      this.openKeys = this.collapsed ? [] : this.openKeysMap[val];
+    }
+  },
+  data() {
+    this.selectedKeysMap = {};
+    this.openKeysMap = {};
+    const menuData = this.getMenuData(this.$router.options.routes);
+    return {
+      collapsed: false,
+      menuData,
+      selectedKeys: this.selectedKeysMap[this.$route.path],
+      openKeys: this.collapsed ? [] : this.openKeysMap[this.$route.path]
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+    getMenuData(routes = [], parentKeys = [], selectedKey) {
+      const menuData = [];
+      routes.forEach(item => {
+        if (item.name && !item.hideInMenu) {
+          this.openKeysMap[item.path] = parentKeys;
+          this.selectedKeysMap[item.path] = [selectedKey || item.path];
+          const newItem = { ...item };
+          delete newItem.children;
+          if (item.children && !item.hideChildrenInMenu) {
+            newItem.children = this.getMenuData(item.children, [
+              ...parentKeys,
+              item.path
+            ]);
+          } else {
+            this.getMenuData(
+              item.children,
+              selectedKey ? parentKeys : [...parentKeys, item.path],
+              selectedKey || item.path
+            );
+          }
+          menuData.push(newItem);
+        } else if (
+          !item.hideInMenu &&
+          !item.hideChildrenInMenu &&
+          item.children
+        ) {
+          menuData.push(
+            ...this.getMenuData(item.children, [...parentKeys, item.path])
+          );
+        }
+      });
+      return menuData;
+    }
+  }
+};
 </script>
-
--- src/layouts/SubMenu.vue
+++ src/layouts/SubMenu.vue
@@ -1,28 +1,29 @@
 <template functional>
-  <a-sub-menu
-    :key="props.menuInfo.key"
-  >
+  <a-sub-menu :key="props.menuInfo.path">
     <span slot="title">
-      <a-icon type="mail" /><span>{{ props.menuInfo.title }}</span>
+      <a-icon
+        v-if="props.menuInfo.meta.icon"
+        :type="props.menuInfo.meta.icon"
+      /><span>{{ props.menuInfo.meta.title }}</span>
     </span>
     <template v-for="item in props.menuInfo.children">
       <a-menu-item
         v-if="!item.children"
-        :key="item.key"
+        :key="item.path"
+        @click="
+          () =>
+            parent.$router.push({ path: item.path, query: parent.$route.query })
+        "
       >
-        <a-icon type="pie-chart" />
-        <span>{{ item.title }}</span>
+        <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
+        <span>{{ item.meta.title }}</span>
       </a-menu-item>
-      <sub-menu
-        v-else
-        :key="item.key"
-        :menu-info="item"
-      />
+      <sub-menu v-else :key="item.path" :menu-info="item" />
     </template>
   </a-sub-menu>
 </template>
 <script>
 export default {
-  props: ['menuInfo'],
+  props: ["menuInfo"]
 };
 </script>
