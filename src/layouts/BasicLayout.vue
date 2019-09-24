3ff8e85e2026c507c2b8febb79314d4ea18a008f
zhangke163 <18435155931@163.com>
2019-09-24 10:34:08 +0800

    精细的权限设计

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,82 +1,85 @@
 <template>
-	<div :class="[`nav-theme-${navTheme}`, `nav-layout-${navLayout}`]">
-		<a-layout id="components-layout-demo-side" style="min-height: 100vh">
-    <a-layout-sider :trigger="null" v-if="navLayout === 'left'" :theme="navTheme"
-      collapsible
-      v-model="collapsed"  width='256px'
-    >
-      <div class="logo">ant design vue pro</div>
-      <SiderMenu :theme="navTheme"/>
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0" >
-		  <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="()=> collapsed = !collapsed"
-        />
-	  <Header/>
-	  </a-layout-header>
-      <a-layout-content style="margin: 0 16px">
-        <router-view></router-view>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        <Footer/>
-      </a-layout-footer>
+  <div :class="[`nav-theme-${navTheme}`, `nav-layout-${navLayout}`]">
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+      <a-layout-sider
+        v-if="navLayout === 'left'"
+        :theme="navTheme"
+        :trigger="null"
+        collapsible
+        v-model="collapsed"
+        width="256px"
+      >
+        <div class="logo">Ant Design Vue Pro</div>
+        <SiderMenu :theme="navTheme" />
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            v-auth="['admin']"
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="collapsed = !collapsed"
+          ></a-icon>
+          <Header />
+        </a-layout-header>
+        <a-layout-content style="margin: 0 16px">
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          <Footer />
+        </a-layout-footer>
+      </a-layout>
     </a-layout>
-  </a-layout>
-  <SettingDrawer/>
-	</div>
+    <Authorized :authority="['admin']">
+      <SettingDrawer />
+    </Authorized>
+  </div>
 </template>
 
 <script>
-	import Header from "./Header.vue"
-	import Footer from "./Footer.vue"
-	import SiderMenu from "./SiderMenu.vue"
-	import SettingDrawer from "../components/SettingDrawer/index.vue"
-	export default{
-		data() {
-			return {
-			  collapsed: false,
-			}
-		  },
-		  computed:{
-		  	navTheme(){
-		  		return this.$route.query.navTheme || 'dark';
-		  	},
-		  	navLayout(){
-		  		return this.$route.query.navLayout || 'left';
-		  	}
-		  },
-		components:{
-			Header,
-			Footer,
-			SiderMenu,
-			SettingDrawer
-		}
-	}
+import Header from "./Header";
+import Footer from "./Footer";
+import SiderMenu from "./SiderMenu";
+import SettingDrawer from "../components/SettingDrawer";
+export default {
+  data() {
+    return {
+      collapsed: false
+    };
+  },
+  computed: {
+    navTheme() {
+      return this.$route.query.navTheme || "dark";
+    },
+    navLayout() {
+      return this.$route.query.navLayout || "left";
+    }
+  },
+  components: {
+    Header,
+    Footer,
+    SiderMenu,
+    SettingDrawer
+  }
+};
 </script>
-<style scoped="scoped">
-#components-layout-demo-side .trigger {
-  font-size: 18px;
+
+<style scoped>
+.trigger {
+  padding: 0 20px;
   line-height: 64px;
-  padding: 0 24px;
-  cursor: pointer;
-  transition: color .3s;
+  font-size: 20px;
 }
-
-#components-layout-demo-side .trigger:hover {
-  color: #1890ff;
+.trigger:hover {
+  background: #eeeeee;
 }
-
-#components-layout-demo-side .logo {
-  height: 32px;
-  background: rgba(255,255,255,.2);
-  margin: 16px;
+.logo {
+  height: 64px;
+  line-height: 64px;
+  text-align: center;
+  overflow: hidden;
 }
-.nav-theme-dark .logo{
-	line-height: 32px;
-	color: #fff;
-	text-align: center;
+.nav-theme-dark >>> .logo {
+  color: #ffffff;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
-import { Button,Layout,Icon,Drawer,Radio,Menu } from 'ant-design-vue';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
+import Authorized from "./components/Authorized";
+import Auth from "./directives/auth";
+
+Vue.config.productionTip = false;
 
 Vue.use(Button);
 Vue.use(Layout);
@@ -10,7 +14,8 @@ Vue.use(Icon);
 Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
-Vue.config.productionTip = false;
+Vue.component("Authorized", Authorized);
+Vue.use(Auth);
 
 new Vue({
   router,
