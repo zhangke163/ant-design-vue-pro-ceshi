95e3535d56f87aeff6007bbbdd5d42a8a46bc1b7
zhangke163 <18435155931@163.com>
2019-09-24 14:46:18 +0800

    v1.2

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,9 +1,56 @@
 <template>
-	<div>分析页</div>
+  <div>
+    <Chart :option="chartOption" style="height: 400px" />
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import Chart from "../../components/Chart";
+export default {
+  data() {
+    return {
+      chartOption: {}
+    };
+  },
+  mounted() {
+    this.getChartData();
+    this.interval = setInterval(() => {
+      this.getChartData();
+    }, 3000);
+  },
+  methods: {
+    getChartData() {
+      axios
+        .get("/api/dashboard/chart", { params: { ID: 12345 } })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: "ECharts 入门示例"
+            },
+            tooltip: {},
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [
+              {
+                name: "销量",
+                type: "bar",
+                data: response.data
+              }
+            ]
+          };
+        });
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+  components: {
+    Chart
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,34 @@ module.exports = {
     loaderOptions: {
       less: {
         modifyVars: {
-          'primary-color': '#1DA57A',
-          'link-color': '#1DA57A',
-          'border-radius-base': '2px',
+          "primary-color": "#1DA57A",
+          "link-color": "#1DA57A",
+          "border-radius-base": "2px"
         },
         javascriptEnabled: true
       }
     }
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        bypass: function(req, res) {
+          if (req.headers.accept.indexOf("html") !== -1) {
+            console.log("Skipping proxy for browser request.");
+            return "/index.html";
+          } else {
+            const name = req.path
+              .split("/api/")[1]
+              .split("/")
+              .join("_");
+            const mock = require(`./mock/${name}`);
+            const result = mock(req.method);
+            delete require.cache[require.resolve(`./mock/${name}`)];
+            return res.send(result);
+          }
+        }
+      }
+    }
   }
-}
+};
