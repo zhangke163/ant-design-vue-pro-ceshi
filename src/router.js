3fc2fcb3d2b6016b0e77299df1b946a0abe8f69f
zhangke163 <18435155931@163.com>
2019-09-24 10:15:41 +0800

    用户菜单权限

--- src/router.js
+++ src/router.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import findLast from "lodash/findLast";
+import { notification } from "ant-design-vue";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import NotFound from "./views/404.vue";
+import NotFound from "./views/404";
+import Forbidden from "./views/403";
+import { check, isLogin } from "./utils/auth";
 
 Vue.use(Router);
 
@@ -11,31 +14,31 @@ const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-		{
-		  path: "/user",
-			hideInMenu:true,
-			component: () =>
-			  import(/* webpackChunkName: "layout" */ "./layouts/UserLayout.vue"),
-		  children:[
-				{
-					path:"/user",
-					redirect:"/user/login"
-				},
-				{
-					path:"/user/login",
-					name:"login",
-					component: () =>
-					  import(/* webpackChunkName: "user" */ "./views/User/Login.vue")
-				},
-				{
-					path:"/user/register",
-					name:"register",
-					component: () =>
-					  import(/* webpackChunkName: "register" */ "./views/User/Register.vue")
-				}
-			]
-		},
-		{
+    {
+      path: "/user",
+      hideInMenu: true,
+      component: () =>
+        import(/* webpackChunkName: "layout" */ "./layouts/UserLayout"),
+      children: [
+        {
+          path: "/user",
+          redirect: "/user/login"
+        },
+        {
+          path: "/user/login",
+          name: "login",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/User/Login")
+        },
+        {
+          path: "/user/register",
+          name: "register",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/User/Register")
+        }
+      ]
+    },
+    {
       path: "/",
       meta: { authority: ["user", "admin"] },
       component: () =>
@@ -57,7 +60,9 @@ const router = new Router({
               name: "analysis",
               meta: { title: "分析页" },
               component: () =>
-                import(/* webpackChunkName: "dashboard" */ "./views/Dashboard/Analysis")
+                import(
+                  /* webpackChunkName: "dashboard" */ "./views/Dashboard/Analysis"
+                )
             }
           ]
         },
@@ -91,19 +96,25 @@ const router = new Router({
                   path: "/form/step-form/info",
                   name: "info",
                   component: () =>
-                    import(/* webpackChunkName: "form" */ "./views/Forms/StepForm/Step1")
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step1"
+                    )
                 },
                 {
                   path: "/form/step-form/confirm",
                   name: "confirm",
                   component: () =>
-                    import(/* webpackChunkName: "form" */ "./views/Forms/StepForm/Step2")
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step2"
+                    )
                 },
                 {
                   path: "/form/step-form/result",
                   name: "result",
                   component: () =>
-                    import(/* webpackChunkName: "form" */ "./views/Forms/StepForm/Step3")
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step3"
+                    )
                 }
               ]
             }
@@ -112,20 +123,47 @@ const router = new Router({
       ]
     },
     {
+      path: "/403",
+      name: "403",
+      hideInMenu: true,
+      component: Forbidden
+    },
+    {
       path: "*",
       name: "404",
-			hideInMenu:true,
+      hideInMenu: true,
       component: NotFound
     }
   ]
 });
-router.beforeEach((to,from,next)=>{
-	if(to.path !== from.path){
-		NProgress.start();
-	}
-	next();
-})
-router.afterEach(()=>{
-	NProgress.done();
-})
+
+router.beforeEach((to, from, next) => {
+  if (to.path !== from.path) {
+    NProgress.start();
+  }
+  const record = findLast(to.matched, record => record.meta.authority);
+  if (record && !check(record.meta.authority)) {
+    if (!isLogin() && to.path !== "/user/login") {
+      next({
+        path: "/user/login"
+      });
+    } else if (to.path !== "/403") {
+      notification.error({
+        message: "403",
+        description: "你没有权限访问，请联系管理员咨询。"
+      });
+      next({
+        path: "/403"
+      });
+    }
+    NProgress.done();
+  }
+
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
+
 export default router;
