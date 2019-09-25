e66209bd02e0b30475b1cd44c8a915e61a44ed9b
zhangke163 <18435155931@163.com>
2019-09-25 13:31:58 +0800

    v1.9

--- src/views/Forms/StepForm/Step3.vue
+++ src/views/Forms/StepForm/Step3.vue
@@ -1,9 +1,9 @@
 <template>
-	<div>Step3</div>
+  <div>操作成功，预计两小时到账</div>
 </template>
 
 <script>
+export default {};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/views/Forms/StepForm/index.vue
+++ src/views/Forms/StepForm/index.vue
@@ -1,9 +1,11 @@
 <template>
-	<div></div>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+export default {};
 </script>
 
-<style>
-</style>
+<style></style>
