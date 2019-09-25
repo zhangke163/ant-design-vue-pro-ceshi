f5ff67ecdfdc43203cb5454e2417ec14dbbe730a
zhangke163 <18435155931@163.com>
2019-09-25 17:44:56 +0800

    自定义

--- src/store/modules/form.js
+++ src/store/modules/form.js
@@ -3,16 +3,20 @@ import request from "../../utils/request";
 
 const state = {
   step: {
-    payAccount: "123456"
+    payAccount: "123456",
+    receiverAccount: {
+      type: "alipay",
+      number: ""
+    }
   }
 };
 
 const actions = {
-  async submitStepForm({ commit }, payload) {
+  async submitStepForm({ commit }, { payload }) {
     await request({
       url: "/api/form",
-      methods: "POST",
-      data: payload.payload
+      method: "POST",
+      data: payload
     });
     commit("saveStepFormData", payload);
     router.push("/form/step-form/result");
